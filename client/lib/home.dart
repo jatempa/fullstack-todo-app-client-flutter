deccafce0cbff720824bd18e2d5475fed91971b2
Jorge Atempa <atempaitm@gmail.com>
2023-02-11 14:52:31 -0800

    Create 'Home' widget

---
+++ client/lib/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const Center(
+        child: Text('Hello World')
+      )
+    );
+  }
+}
