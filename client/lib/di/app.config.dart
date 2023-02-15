6eb6b1e6f51068a403a8275029fc4eed43f8e8f7
Jorge Atempa <atempaitm@gmail.com>
2023-02-14 18:31:23 -0800

    Setup dependency injection library

---
+++ client/lib/di/app.config.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:client/data/data_sources/TaskRemoteDataProvider.dart' as _i3;
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:injectable/injectable.dart' as _i2;
+
+// ignore_for_file: unnecessary_lambdas
+// ignore_for_file: lines_longer_than_80_chars
+// initializes the registration of main-scope dependencies inside of GetIt
+_i1.GetIt init(
+  _i1.GetIt getIt, {
+  String? environment,
+  _i2.EnvironmentFilter? environmentFilter,
+}) {
+  final gh = _i2.GetItHelper(
+    getIt,
+    environment,
+    environmentFilter,
+  );
+  gh.lazySingleton<_i3.TaskRemoteDataProvider>(
+      () => _i3.TaskRemoteDataProvider());
+  return getIt;
+}
---
+++ client/lib/di/app.dart
@@ -0,0 +1,12 @@
+import 'package:client/di/app.config.dart';
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+final getIt = GetIt.instance;  
+  
+@InjectableInit(  
+  initializerName: 'init', // default  
+  preferRelativeImports: true, // default  
+  asExtension: false, // default  
+)  
+void configureDependencies() => init(getIt);
