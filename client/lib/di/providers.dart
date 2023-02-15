a3420c612920f2a9942da5b776262b87d9b0a33e
Jorge Atempa <atempaitm@gmail.com>
2023-02-15 13:46:33 -0800

    Create provider to fetch all tasks

--- client/lib/di/providers.dart
+++ client/lib/di/providers.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:client/data/data_sources/TaskRemoteDataProvider.dart';
 import 'package:client/data/repositories/TaskRepository.dart';
+import 'package:client/domain/entities/Task.dart';
 import 'package:client/domain/services/TaskService.dart';
 
 final taskRemoteDataProvider = Provider<TaskRemoteDataProvider>((ref) {
@@ -16,4 +17,9 @@ final taskRepositoryProvider = Provider<TaskRepository>((ref) {
 final taskServiceProvider = Provider<TaskService>((ref) {
   final repositoryProvider = ref.watch(taskRepositoryProvider);
   return TaskService(repository: repositoryProvider);
+});
+
+final tasksProvider = FutureProvider<List<Task>?>((ref) {
+  final serviceProvider = ref.watch(taskServiceProvider);
+  return serviceProvider.fetchAll();
 });
