ac52c20f018236cbff877bb34964faf74892d083
Jorge Atempa <atempaitm@gmail.com>
2023-02-15 16:48:00 -0800

    Re-render list of tasks after select one item

--- client/lib/widgets/task_item.dart
+++ client/lib/widgets/task_item.dart
@@ -1,31 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:client/di/providers.dart';
 import 'package:client/domain/entities/Task.dart';
+import 'package:client/widgets/status.dart';
 
-class TaskItem extends StatelessWidget {
+class TaskItem extends ConsumerWidget {
   final Task? task;
 
   const TaskItem({super.key, this.task});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Card(
       margin: const EdgeInsets.all(8.0),
       child: ListTile(
+        onTap: () async {
+          await ref.read(taskServiceProvider).update(task?.id);
+          ref.refresh(tasksProvider.future);
+        },
         title: Text(
           '${task?.title}',
           style: const TextStyle(
             fontWeight: FontWeight.bold
           )
         ),
-        trailing: _buildStatusIndicator(task),
+        trailing: Status(status: task?.done)
       )
     );
-  }
-
-  _buildStatusIndicator(Task? task) {
-    return (task != null && task.done!)
-    ? Container(color: Colors.green, width: 25, height: 25)
-    : Container(color: Colors.grey, width: 25, height: 25);
   }
 }
