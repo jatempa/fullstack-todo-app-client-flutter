72ce3f628ddbbe7c0a92863666571a71a03e5bb0
Jorge Atempa <atempaitm@gmail.com>
2023-11-16 19:23:46 -0800

    Update title

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -17,11 +17,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'To Do',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'To Do'),
     );
   }
 }
